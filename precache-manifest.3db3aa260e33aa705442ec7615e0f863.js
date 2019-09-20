a30fbb26385d2696f4004b55f8c7e0ec26082e9e
Perry Vessels <168404+pvdev@users.noreply.github.com>
2019-09-20 06:55:38 -0400

    Updates

---
+++ precache-manifest.3db3aa260e33aa705442ec7615e0f863.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb0f0ecc5479755727c04dabec55c789",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "565606e8e05d471fdfaf",
+    "url": "/monsters-rolodex/static/css/main.67ca950b.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "565606e8e05d471fdfaf",
+    "url": "/monsters-rolodex/static/js/main.539d35fe.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
