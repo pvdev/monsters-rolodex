6e259369fe7ba25ce6800107791a57210038ead2
Perry Vessels <168404+pvdev@users.noreply.github.com>
2019-09-20 07:07:11 -0400

    adding project files completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super()
 
-export default App;
+    this.state = {
+      monsters: [ ],
+      searchField: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}))
+  }
+
+  // takes advantage of lexically scoped arrow function setting 'this' to 
+  // the component otherwise we'd have to bind this in the constructor
+  handleChange = e => this.setState({ searchField: e.target.value })
+
+  render() {
+    const { monsters, searchField } = this.state
+    const filteredMonsters = monsters.filter( monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+        )
+    return (
+      <div className="App">
+        <h1> Monster's Rolodex </h1>
+        <SearchBox 
+          placeholder='search monsters' 
+          handleChange={ this.handleChange } 
+        />
+        <CardList monsters = {filteredMonsters} />
+      </div>
+    )
+  }
+} //end component
+
+
+  export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './search-box.styles.css'
+
+// sets up for event based state updates
+export const SearchBox = ({ placeholder, handleChange }) => (
+    <input 
+        className='search'
+        type='search' 
+        placeholder={ placeholder }
+        onChange={ handleChange }
+    />
+)
