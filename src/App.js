573975cf7376da0feab33acba68f8cdd7bbe75e0
Perry Vessels <168404+pvdev@users.noreply.github.com>
2019-09-21 11:25:46 -0400

    comments added and setstate example with function

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import React, { Component } from "react";
-import "./App.css";
-import { CardList } from "./components/card-list/card-list.component";
-import { SearchBox } from "./components/search-box/search-box.component";
+import React, { Component } from "react"
+import "./App.css"
+import { CardList } from "./components/card-list/card-list.component"
+import { SearchBox } from "./components/search-box/search-box.component"
 
 class App extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
       monsters: [],
       searchField: "",
-    };
+    }
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
-      .then(users => this.setState({ monsters: users }));
+      .then(users => this.setState({ monsters: users }))
   }
 
   // takes advantage of lexically scoped arrow function setting 'this' to
   // the component otherwise we'd have to bind this in the constructor
-  handleChange = e => this.setState({ searchField: e.target.value });
+  //handleChange = e => this.setState({ searchField: e.target.value });
+
+  // done with previous state & props function call
+  handleChange = e =>
+    this.setState((prevState, prevProps) => {
+      return { searchField: e.target.value }
+    })
 
   render() {
-    const { monsters, searchField } = this.state;
+    const { monsters, searchField } = this.state
     const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
+    )
     return (
       <div className="App">
         <h1> Monster's Rolodex </h1>
@@ -37,8 +43,8 @@ class App extends Component {
         />
         <CardList monsters={filteredMonsters} />
       </div>
-    );
+    )
   }
 } //end component
 
-export default App;
+export default App
